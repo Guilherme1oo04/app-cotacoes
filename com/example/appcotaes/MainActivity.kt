583387316a92d103c0eab79d5fa6dc47f9b72ac0
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-04 19:26:19 -0300

    App initialization

---
+++ com/example/appcotaes/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.appcotaes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.appcotaes.ui.interfaces.Screen
+import com.example.appcotaes.ui.theme.AppCotaçõesTheme
+import com.example.appcotaes.utils.InternetDevice
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppCotaçõesTheme {
+                Surface(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    Screen(internetDevice = InternetDevice(LocalContext.current))
+                }
+            }
+        }
+    }
+}
+
