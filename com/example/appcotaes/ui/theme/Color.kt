537fe3ad0da993903b856d7d48b6eb63a9d3307c
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-04 19:29:07 -0300

    Tema da aplicação

---
+++ com/example/appcotaes/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.appcotaes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val FrenchGray = Color(0xFFB8B8D1)
+val Blue = Color(0xFF32599E)
+val LightGreen = Color(0xFF80B192)
+val GiantOrange = Color(0xFFF26430)
+val Red = Color(0xFFFF1B1C)
---
+++ com/example/appcotaes/ui/theme/Theme.kt
@@ -0,0 +1,52 @@
+package com.example.appcotaes.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val ColorScheme = darkColorScheme(
+        primary = Blue,
+        secondary = LightGreen,
+        tertiary = GiantOrange,
+        background = FrenchGray,
+        error = Red,
+)
+
+        /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+
+@Composable
+fun AppCotaçõesTheme(
+        darkTheme: Boolean = isSystemInDarkTheme(),
+        content: @Composable () -> Unit
+) {
+    val colorScheme = ColorScheme
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+    )
+}
