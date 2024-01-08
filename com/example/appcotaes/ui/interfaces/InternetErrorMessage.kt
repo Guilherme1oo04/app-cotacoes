dac14cd7c832e1cdf748ce4168eca3b88f8bcf58
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-08 17:44:31 -0300

    Modificando tela de sem conexão

--- com/example/appcotaes/ui/interfaces/InternetErrorMessage.kt
+++ com/example/appcotaes/ui/interfaces/InternetErrorMessage.kt
@@ -1,21 +1,39 @@
 package com.example.appcotaes.ui.interfaces
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun InternetErrorMessage(internetVerification: () -> Unit) {
+    var isPressed by remember {
+        mutableStateOf(false)
+    }
+    val buttonColor = when(isPressed) {
+        true -> MaterialTheme.colorScheme.onPrimary
+        else -> MaterialTheme.colorScheme.primary
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -24,12 +42,21 @@ fun InternetErrorMessage(internetVerification: () -> Unit) {
         Text(
             text = "Sem conexão com a internet!",
             modifier = Modifier.padding(16.dp),
-            fontSize = 28.sp
+            fontSize = 28.sp,
+            textAlign = TextAlign.Center
         )
 
         Button(onClick = {
             internetVerification()
-        }) {
+
+            isPressed = true
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                isPressed = false
+            }, 500)
+        },
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+        ) {
             Text(
                 text = "Tentar novamente",
                 color = Color.White,
