f4b7347fbc94ce916da5382ebd19c1adc04f7cd2
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-09 15:58:30 -0300

    Criando classe de instância da lib retrofit

---
+++ com/example/appcotaes/utils/InstanceRetrofit.kt
@@ -0,0 +1,16 @@
+package com.example.appcotaes.utils
+
+import com.example.appcotaes.utils.interfaces.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class InstanceRetrofit {
+    private val retrofit =  Retrofit.Builder()
+        .baseUrl(COTACOES_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun createService(): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
