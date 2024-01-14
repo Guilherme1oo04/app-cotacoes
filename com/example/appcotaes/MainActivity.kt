a87fc446930277f30cb5df8bc718e3c669031380
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-14 18:15:56 -0300

    Retirando importações não utilizadas

--- com/example/appcotaes/MainActivity.kt
+++ com/example/appcotaes/MainActivity.kt
@@ -3,15 +3,10 @@ package com.example.appcotaes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.appcotaes.ui.interfaces.Screen
 import com.example.appcotaes.ui.theme.AppCotaçõesTheme
 import com.example.appcotaes.utils.InternetDevice
-import com.example.appcotaes.utils.StateCoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
