1cbfe89f60a03c101e3a2e794497387085d22d6a
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-28 19:54:27 -0300

    Atualização de interface

--- com/example/appcotaes/ui/interfaces/App.kt
+++ com/example/appcotaes/ui/interfaces/App.kt
@@ -1,8 +1,10 @@
 package com.example.appcotaes.ui.interfaces
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -42,55 +44,57 @@ fun App(coinsList: List<CoinModel>) {
         mutableStateOf(false)
     }
 
-    Column(modifier = Modifier
-        .clickable(interactionSource = MutableInteractionSource(), indication = null) {
-            if (expandedPopupMoeda1) {
-                expandedPopupMoeda1 = false
+    Box {
+        Column(modifier = Modifier
+            .clickable(interactionSource = MutableInteractionSource(), indication = null) {
+                if (expandedPopupMoeda1) {
+                    expandedPopupMoeda1 = false
+                }
+                if (expandedPopupMoeda2) {
+                    expandedPopupMoeda2 = false
+                }
             }
-            if (expandedPopupMoeda2) {
-                expandedPopupMoeda2 = false
-            }
-        }
-        .fillMaxHeight()) {
+            .fillMaxHeight()) {
 
-        Text(
-            text = "Espia Câmbio",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            textAlign = TextAlign.Center,
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary,
-            style = TextStyle(
-                shadow = Shadow(
-                    color = Color.Black,
-                    offset = Offset(1.5f, 1.5f),
-                    blurRadius = 0f
+            Text(
+                text = "Espia Câmbio",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                style = TextStyle(
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(1.5f, 1.5f),
+                        blurRadius = 0f
+                    )
                 )
             )
-        )
 
-        Row(
-            modifier = Modifier
-                .padding(13.dp)
-        ) {
-            Text(
+            Row(
                 modifier = Modifier
-                    .clickable(onClick = {
-                        expandedPopupMoeda1 = !expandedPopupMoeda1
-                    }, onClickLabel = "")
-                    .border(
-                        width = 2.dp,
-                        color = MaterialTheme.colorScheme.primary,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .padding(5.dp),
-                text = moeda1Selecionada.sigla,
-                fontSize = 22.sp,
-            )
+                    .padding(13.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .clickable{
+                            expandedPopupMoeda1 = !expandedPopupMoeda1
+                        }
+                        .padding(5.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.background,
+                            shape = RoundedCornerShape(10.dp)
+                        ),
+                    text = moeda1Selecionada.sigla,
+                    fontSize = 22.sp,
+                )
+            }
         }
 
+        /* Moeda 1 */
         CoinSelectionList(
             coinsList = coinsList,
             showDialog = expandedPopupMoeda1,
