07c6368607ed2e7ce4a828a3023615214336722a
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-01-28 19:53:53 -0300

    Atualização da seleção de moeda

--- com/example/appcotaes/ui/interfaces/CoinSelectionList.kt
+++ com/example/appcotaes/ui/interfaces/CoinSelectionList.kt
@@ -1,15 +1,19 @@
 package com.example.appcotaes.ui.interfaces
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import com.example.appcotaes.data.CoinModel
 
 @Composable
@@ -20,14 +24,30 @@ fun CoinSelectionList(
     closeDialog: () -> Unit
 ) {
     if (showDialog) {
-        Dialog(onDismissRequest = { closeDialog() }) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = MaterialTheme.colorScheme.background,
+                )
+                .padding(top = 10.dp, bottom = 10.dp),
+        ) {
             LazyColumn {
                 items(coinsList) {moeda ->
-                    Text(text = moeda.nome, modifier = Modifier.clickable {
-                        labelFunction(moeda)
-                        closeDialog()
-                    })
-                    Spacer(modifier = Modifier.height(50.dp))
+                    Box(
+                        modifier = Modifier.height(65.dp)
+                    ) {
+                        Text(
+                            text = moeda.nome,
+                            modifier = Modifier
+                                .clickable {
+                                    labelFunction(moeda)
+                                    closeDialog()
+                                }
+                                .padding(horizontal = 10.dp)
+                                .align(Alignment.Center)
+                        )
+                    }
                 }
             }
         }
