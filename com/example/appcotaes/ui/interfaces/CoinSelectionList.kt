077182853c0323b71ce3451d45270b7e99e65488
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-02-06 18:16:49 -0300

    Criando função de pesquisa para as moedas

--- com/example/appcotaes/ui/interfaces/CoinSelectionList.kt
+++ com/example/appcotaes/ui/interfaces/CoinSelectionList.kt
@@ -2,20 +2,26 @@ package com.example.appcotaes.ui.interfaces
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,18 +30,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.appcotaes.data.CoinModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinSelectionList(
     coinsList: List<CoinModel>,
     showDialog: Boolean,
     moedaSelecionada: CoinModel,
     labelFunction: (CoinModel) -> Unit,
-    closeDialog: () -> Unit
+    closeDialog: () -> Unit,
+    pesquisaQuery: String,
+    pesquisaQueryFunction: (String) -> Unit
 ) {
 
     val itemSelecionado = coinsList.find { it.nome == moedaSelecionada.nome && it.sigla == moedaSelecionada.sigla }
     var selectedIndexItem = coinsList.indexOf(itemSelecionado)
     val listState = rememberLazyListState()
+
+
+    val filteredCoinsList = coinsList.filter {
+        it.nome.contains(pesquisaQuery, ignoreCase = true)
+    }
 
     if (showDialog) {
         Box(
@@ -47,19 +61,41 @@ fun CoinSelectionList(
                 .padding(top = 10.dp, bottom = 10.dp),
         ) {
             Column {
-                Icon(
-                    Icons.Rounded.Close,
-                    contentDescription = null,
+                Row(
                     modifier = Modifier
-                        .clickable {
-                            closeDialog()
+                        .padding(top = 5.dp, start = 10.dp, bottom = 5.dp, end = 15.dp)
+                        .height(60.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Rounded.Close,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clickable {
+                                closeDialog()
+                                pesquisaQueryFunction("")
+                            }
+                            .size(40.dp),
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                    OutlinedTextField(
+                        value = pesquisaQuery,
+                        onValueChange = {
+                            pesquisaQueryFunction(it)
+                        },
+                        modifier = Modifier
+                            .width(250.dp),
+                        placeholder = {
+                            Row {
+                                Icon(Icons.Rounded.Search, contentDescription = null, tint = MaterialTheme.colorScheme.secondary)
+                            }
                         }
-                        .padding(top = 5.dp, start = 10.dp, bottom = 5.dp)
-                        .size(40.dp),
-                    tint = MaterialTheme.colorScheme.secondary
-                )
+                    )
+                }
                 LazyColumn(state = listState) {
-                    items(coinsList) {moeda ->
+                    items(filteredCoinsList) {moeda ->
                         val indexItem = coinsList.indexOf(moeda)
                         val color =
                             if(moeda.sigla == moedaSelecionada.sigla && moeda.nome == moedaSelecionada.nome) MaterialTheme.colorScheme.primary
@@ -72,6 +108,7 @@ fun CoinSelectionList(
                                 .background(color)
                                 .clickable {
                                     labelFunction(moeda)
+                                    pesquisaQueryFunction("")
                                     closeDialog()
                                 }
                         ) {
